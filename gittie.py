b3bc8140054b1574f364e6125bbce30dfdd385c3
Alex Lisztes <lisztes.alex@gmail.com>
2017-11-15 09:40:50 +0100

    Merge conflict solved.

--- gittie.py
+++ gittie.py
@@ -1,12 +1,10 @@
 def introduce():
     print("Hello, I'm Gittie!")
 
-<<<<<<< HEAD
+
 def joke():
     print("What is the difference between a snowman and a snowwoman? - Snowballs.")
-=======
 
 def add(a, b):
     return a + b
->>>>>>> 45a736d2c69838856f322b7abfb74dd5860628a2
 
